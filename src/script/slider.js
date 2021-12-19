5e5dbdccf1a518d10697a0d6aad0cc68bc87a079
Ira Redchets <irared95@gmail.com>
2021-12-19 22:00:21 +0300

    start project

--- src/script/slider.js
+++ src/script/slider.js
@@ -1,3 +1,32 @@
-const slider = document.querySelector('.slider')
-console.log('Slider.js File')
-console.log('Slider.js File')
+// document.addEventListener('DOMContentLoaded', function() {
+
+//     $('.products__slider--js').slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         autoplay: false,
+//         speed: 300,
+//         arrows: true,
+//         prevArrow: '<div class="products__arrow-prev products__arrow"><img src="./image/arrow-green-left.svg" alt="arrow"></div>',
+//         nextArrow: '<div class="products__arrow-next products__arrow"><img src="./image/arrow-green-right.svg" alt="arrow"></div>',
+
+//         responsive: [{
+//                 breakpoint: 1024,
+//                 settings: {
+//                     slidesToShow: 3
+//                 }
+//             },
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 2
+//                 }
+//             },
+//             {
+//                 breakpoint: 500,
+//                 settings: {
+//                     slidesToShow: 1
+//                 }
+//             }
+//         ]
+//     });
+// });

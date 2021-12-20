eb6b65553f37ef93d5f6fba1f78ecdca1750ffcb
Ira Redchets <irared95@gmail.com>
2021-12-21 00:09:01 +0200

    done new Menu section, started review section

--- src/script/slider.js
+++ src/script/slider.js
@@ -1,32 +1,32 @@
-// document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
 
-//     $('.products__slider--js').slick({
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         autoplay: false,
-//         speed: 300,
-//         arrows: true,
-//         prevArrow: '<div class="products__arrow-prev products__arrow"><img src="./image/arrow-green-left.svg" alt="arrow"></div>',
-//         nextArrow: '<div class="products__arrow-next products__arrow"><img src="./image/arrow-green-right.svg" alt="arrow"></div>',
+    $('.products__slider--js').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        speed: 300,
+        arrows: false,
+        // prevArrow: '<div class="products__arrow-prev products__arrow"><img src="./image/arrow-green-left.svg" alt="arrow"></div>',
+        // nextArrow: '<div class="products__arrow-next products__arrow"><img src="./image/arrow-green-right.svg" alt="arrow"></div>',
 
-//         responsive: [{
-//                 breakpoint: 1024,
-//                 settings: {
-//                     slidesToShow: 3
-//                 }
-//             },
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     slidesToShow: 2
-//                 }
-//             },
-//             {
-//                 breakpoint: 500,
-//                 settings: {
-//                     slidesToShow: 1
-//                 }
-//             }
-//         ]
-//     });
-// });
+        // responsive: [{
+        //         breakpoint: 1024,
+        //         settings: {
+        //             slidesToShow: 3
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 768,
+        //         settings: {
+        //             slidesToShow: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 500,
+        //         settings: {
+        //             slidesToShow: 1
+        //         }
+        //     }
+        // ]
+    });
+});

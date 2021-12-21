e20d87db7a00f1680dc5ff1dc4254f99ff6891c0
Ira Redchets <irared95@gmail.com>
2021-12-22 00:07:26 +0200

    done reservation section and added some fixes in reviews section

--- src/js/script.js
+++ src/js/script.js
@@ -10886,27 +10886,54 @@ document.addEventListener('DOMContentLoaded', function() {
         autoplay: false,
         speed: 300,
         arrows: false,
-        // prevArrow: '<div class="products__arrow-prev products__arrow"><img src="./image/arrow-green-left.svg" alt="arrow"></div>',
-        // nextArrow: '<div class="products__arrow-next products__arrow"><img src="./image/arrow-green-right.svg" alt="arrow"></div>',
-
-        // responsive: [{
-        //         breakpoint: 1024,
-        //         settings: {
-        //             slidesToShow: 3
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 768,
-        //         settings: {
-        //             slidesToShow: 2
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 500,
-        //         settings: {
-        //             slidesToShow: 1
-        //         }
-        //     }
-        // ]
     });
+
+    //slider reviews
+    $('.reviews__slider--js').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: false,
+        speed: 300,
+        arrows: true,
+        prevArrow: '<div class="reviews__arrow-prev reviews__arrow">' +
+                '<svg xmlns="http://www.w3.org/2000/svg" stroke="white"  width="24" height="24" viewBox="0 0 24 24" fill="none">\n' +
+                '\t<g clip-path="url(#clip0_1_180)">\n' +
+                '\t\t<path d="M23 12H5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+                '\t\t<path d="M12 19L5 12L12 5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+                '\t</g>\n' +
+                '\t<defs>\n' +
+                '\t\t<clipPath id="clip0_1_180">\n' +
+                '\t\t\t<rect width="24" height="24" fill="white" transform="translate(24 24) rotate(180)"/>\n' +
+                '\t\t</clipPath>\n' +
+                '\t</defs>\n' +
+                '</svg>' +
+            '</div>',
+        nextArrow: '<div class="reviews__arrow-next reviews__arrow">' +
+                '<svg xmlns="http://www.w3.org/2000/svg" stroke="white" width="24" height="24" viewBox="0 0 24 24" fill="none">\n' +
+                '\t<path d="M1 12.0001L19 12.0001" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+                '\t<path d="M12 5.00012L19 12.0001L12 19.0001" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+                '</svg>' +
+            '</div>',
+
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    })
 });
+
